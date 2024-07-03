66ca7ca9fccdc6a29130bf79bcf430804b346eb4
Larnell Bentley <lbentley@Larnells-MacBook-Pro.local>
2024-07-02 19:05:17 -0700

    included async & promises

--- index.js
+++ index.js
@@ -4,9 +4,33 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
 
+function waitAndLog(Time, DoneWalking) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(DoneWalking);
+            resolve();
+        }, 3000);
+    });
+}
+
+// have the NPC start walking east immediately
+npc.walkEast(1000)
+    .then(() => waitAndLog(1400, "Done walking east"))
+    .then(() => npc.walkEast(1400))
+    .then(() => waitAndLog(300, "Done walking south"))
+    .then(() => npc.walkSouth(300))
+    .then(() => waitAndLog(1500, "Done walking east again"))
+    .then(() => npc.walkEast(1500))
+    .then(() => waitAndLog(800, "Done walking south again"))
+    .then(() => npc.walkSouth(800))
+    .then(() => waitAndLog(2400, "Done walking west"))
+    .then(() => npc.walkWest(2400))
+    .then(() => waitAndLog(2300, "Done walking north"))
+    .then(() => npc.walkNorth(2300))
+    .catch(error => {
+        console.log('Error during NPC movement:', error);
+    });
 // Create the inventory
 const inventory = newInventory()
 move(inventory).to(0, 0)
@@ -20,3 +44,5 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,51 +1,76 @@
 function newNonPlayableCharacter(x, y) {
-    let element = newImage('assets/red-character/static.gif')
+    let element = newImage('assets/red-character/static.gif');
     element.style.zIndex = 1;
     
     let direction = null;
 
     function moveCharacter() {
         if (direction === 'west') {
-            x -= 1
+            x -= 1;
         }
         if (direction === 'north') {
-            y += 1
+            y += 1;
         }
         if (direction === 'east') {
-            x += 1
+            x += 1;
         }
         if (direction === 'south') {
-            y -= 1
+            y -= 1;
         }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
     }
 
-    setInterval(moveCharacter, 1)
+    setInterval(moveCharacter, 1);
 
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
+    function walkEast(time) {
+        return new Promise(resolve => {
+            direction = 'east';
+            element.src = `./assets/red-character/east.gif`;
+            setTimeout(() => {
+                stop();
+                resolve(); // Resolve the promise after the specified time
+            }, time);
+        });
     }
 
-    function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+    function walkNorth(time) {
+        return new Promise(resolve => {
+            direction = 'north';
+            element.src = `./assets/red-character/north.gif`;
+            setTimeout(() => {
+                stop();
+                resolve();
+            }, time);
+        });
     }
 
-    function walkWest() {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
+    function walkWest(time) {
+        return new Promise(resolve => {
+            direction = 'west';
+            element.src = `./assets/red-character/west.gif`;
+            setTimeout(() => {
+                stop();
+                resolve();
+            }, time);
+        });
     }
 
-    function walkSouth() {
-        direction = 'south'
-        element.src = `./assets/red-character/south.gif`
+    function walkSouth(time) {
+        return new Promise(resolve => {
+            direction = 'south';
+            element.src = `./assets/red-character/south.gif`;
+            setTimeout(() => {
+                stop();
+                resolve();
+            }, time);
+        });
     }
 
     function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
+        direction = null;
+        
+        element.src = `./assets/red-character/static.gif`;
     }
 
     return {
@@ -55,5 +80,5 @@ function newNonPlayableCharacter(x, y) {
         walkEast: walkEast,
         walkSouth: walkSouth,
         stop: stop
-    }
+    };
 }
